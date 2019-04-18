cc1068b2f3201ec0bd250ae27eeebd1ed85c2263
Chris Druta <cndruta@email.arizona.edu>
2019-04-17 20:44:58 -0700

    Added sav-trip matching

--- main.py
+++ main.py
@@ -21,46 +21,40 @@ def plot(geopandaMap):
     """
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
-    plot_polygon_collection(ax, geopandaMap['geometry'], geopandaMap['parking_demand'])
+    plot_polygon_collection(ax, geopandaMap.geometry, geopandaMap['parking_demand'])
     plt.axis('off')
 
 zoneIdMap = parser.readZoneIdMap()
 
-# Initate trips
+print("Generating trips...")
 trips, zoneDist = parser.generateTripsAndZoneDist("output.csv", 1, 0.02)
 print(f"Number of trips: {len(trips)}")
 
-# Iniate SAVs
+# Read in nyc zone map
+zoneMap = geopandas.read_file('taxi_zones/taxi_zones.shp')
+
+# Initate SAVs
 # What is the fleet size we should use?
 n = 500
-controller = VehicleController(n, zoneDist)
-
-zoneMap = geopandas.read_file('taxi_zones/taxi_zones.shp')
+controller = VehicleController(n, zoneDist, zoneMap)
+print("Finished setting up model controller")
 
 # Initial distribution of vehicles
 colors = {k: 0 for k in range(1, zoneMap.shape[0] + 1)}
-for vehicle in controller.allVehicles:
+for vehicle in controller.parkedVehicles:
     colors[vehicle.currentZone] += 1
 
 zoneMap['parking_demand'] = list(colors.values())
-plot(zoneMap)
 
-plt.show()
-quit()
-
+print("Starting simulation day")
 for hour in range(24):
     hourTrips = trips[trips[:,0] == hour]
 
     for minute in range(60):
+
         tripsToStart = hourTrips[hourTrips[:,1] == minute]
-        #print(tripsToStart) comments
-        
-        # Match SAVs to trips and use google api destination matrix for travel times
-        #   Remove matched SAVs from availibile SAVs
+        controller.matchVehicles(tripsToStart, (hour, minute))
 
         # Check if any trips were completed and add SAVs back to available SAVs with new location
 
-        # Keep track of roaming SAVs => Note: Available SAVs = Parked and roaming SAVs
-
-        pass
-        
+print(len(controller.availableVehicles))
--- vehicle.py
+++ vehicle.py
@@ -9,32 +9,74 @@ class Vehicle():
 
     def __init__(self, zoneId):
         self.currentZone = zoneId
-        self.destinationZone = 0
+        self.nextZone = 0
         self.route = []
+        self.tripStartHour = 0
+        self.tripStartMinute = 0
+    
+    def getCurrentZone(self, timeInTrip):
+        # if route length is 0, has no route => is parked
+        if len(self.route) == 0:
+            return self.currentZone
+        else:
+            return -1 # From google maps route, convert long lat to zone id on map w/ geopanda
 
 class VehicleController():
 
-    def __init__(self, n, zoneDist):
+    def __init__(self, n, zoneDist, zoneMap):
         self.fleetSize = n
         self.zoneDist = zoneDist
+        self.zoneMap = zoneMap
+        self.zoneCentroids = {(i + 1): p for i, p in enumerate(zoneMap['geometry'].centroid)}
 
-        self.availableVehicles = []
-        self.travelingVehicles = []
+        self.roamingVehicles = []
+        self.parkedVehicles = []
+        self.drivingVehicles = []
 
-        sample = np.random.choice(list(self.zoneDist.keys()), size=self.fleetSize, p=list(self.zoneDist.values()))
+        # Give all parked vehicles initial positions
+        sample = np.random.choice(list(self.zoneDist.keys()),
+                                    size=self.fleetSize,
+                                    p=list(self.zoneDist.values()))
         for i in range(self.fleetSize):
-            self.availableVehicles.append(Vehicle(sample[i]))
+            self.parkedVehicles.append(Vehicle(sample[i]))
 
     def __str__(self):
-        return '\n'.join(f"{i}: {vehicle.currentZone}" for i, vehicle in enumerate(self.availableVehicles))
-
+        return '\n'.join(f"{i}: {vehicle.currentZone}" for i, vehicle in enumerate(self.allVehicles))
 
     @property
-    def allVehicles(self):
-        return self.availableVehicles + self.travelingVehicles
+    def availableVehicles(self):
+        return self.roamingVehicles + self.parkedVehicles
 
     def updateVehicles(self):
         pass
 
-    def matchVehicles(self, trips):
-        pass
+    def matchVehicles(self, trips, t):
+     
+        for trip in trips:
+            # Find nearest available SAV
+            bestDistance = 100000
+            bestSav = None
+            for sav in self.availableVehicles:
+                timeInTrip = (t[0] - sav.tripStartHour, t[1] - sav.tripStartMinute)
+                distance = self.zoneCentroids[sav.getCurrentZone(timeInTrip)].distance(self.zoneCentroids[trip[2]])
+                # TODO: Handle scenerio where sav current zone = trip pick up zone
+                if distance < bestDistance:
+                    bestDistance = distance
+                    bestSav = sav
+
+            if bestSav == None:
+                print("Matching center didn't match trip with vehicle")
+                continue
+                #raise RuntimeError("Matching center didn't match trip with vehicle")
+                # TODO: Actually handle this scenerio => doublecheck
+
+            # Updated matched sav
+            bestSav.nextZone = trip[3]
+            self.drivingVehicles.append(bestSav)
+
+            # Remove from available vehicles
+            print("Trip matched!")
+            try:
+                self.roamingVehicles.remove(bestSav)
+            except:
+                self.parkedVehicles.remove(bestSav)
