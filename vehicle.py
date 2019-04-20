b8cf6cea9d217ef7ca31689e8bfb096b74e4defb
Chris Druta <cndruta@email.arizona.edu>
2019-04-20 01:38:07 -0700

    Updated matching for same zone case

--- vehicle.py
+++ vehicle.py
@@ -1,4 +1,4 @@
-
+import math
 import numpy as np
 
 import parser
@@ -12,6 +12,7 @@ class Vehicle:
         self.route = []
         self.tripStartHour = 0
         self.tripStartMinute = 0
+        self.tripLength = 0
     
     def getCurrentZone(self, timeInTrip):
         # if route length is 0, has no route => is parked
@@ -50,6 +51,8 @@ class VehicleController:
         pass
 
     def matchVehicles(self, trips, t):
+
+        googleMapsBuffer = []
      
         for trip in trips:
             # Find nearest available SAV
@@ -65,18 +68,35 @@ class VehicleController:
                     bestSav = sav
 
             if bestSav == None:
-                print("Matching center didn't match trip with vehicle")
+                #print("Matching center didn't match trip with vehicle")
                 continue
                 #raise RuntimeError("Matching center didn't match trip with vehicle")
                 # TODO: Actually handle this scenerio => doublecheck
 
             # Updated matched sav
             bestSav.nextZone = trip[3]
-            self.drivingVehicles.append(bestSav)
+            bestSav.tripStartHour = t[0]
+            bestSav.tripStartMinute = t[1]
 
+            if bestDistance == 0:
+                # randomly sample average centroid radius to get estimated pick up time
+                bestSav.tripLength = math.ceil(np.random.uniform() * self.zoneRadiusMap[trip[2]] / 17.6 * 60)
+                self.drivingVehicles.append(bestSav)
+            else:
+                # use google maps
+                googleMapsBuffer.append(bestSav)
+                
             # Remove from available vehicles
             print("Trip matched!")
             try:
                 self.roamingVehicles.remove(bestSav)
             except:
                 self.parkedVehicles.remove(bestSav)
+
+        # Construct 1 api request (destination matrix) to google maps
+        for sav in googleMapsBuffer:
+            pass
+
+        # Send and await response
+
+        self.drivingVehicles.extend(googleMapsBuffer)
