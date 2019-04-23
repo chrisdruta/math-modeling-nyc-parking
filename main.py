034e0ab49c2c58732f717803128bcb7f951feb93
Chris Druta <cndruta@email.arizona.edu>
2019-04-22 18:06:35 -0700

    Added parking demand calculation

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def plot(geopandaMap):
     """
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
-    plt.hold(True)
     plot_polygon_collection(ax, geopandaMap.geometry, geopandaMap['parking_demand'])
     plt.axis('off')
 
@@ -40,11 +39,11 @@ controller = VehicleController(n, zoneDist)
 print("Finished setting up model controller")
 
 # Initial distribution of vehicles
-colors = {k: 0 for k in range(1, zoneMap.shape[0] + 1)}
+initialDist = {k: 0 for k in zoneIdMap.keys()}
 for vehicle in controller.parkedVehicles:
-    colors[vehicle.currentZone] += 1
+    initialDist[vehicle.currentZone] += 1
 
-zoneMap['parking_demand'] = list(colors.values())
+parkingDemand = {k: 0 for k in zoneIdMap.keys()}
 
 print("Starting simulation day")
 for hour in range(24):
@@ -55,6 +54,10 @@ for hour in range(24):
         tripsToStart = hourTrips[hourTrips[:,1] == minute]
         controller.matchVehicles(tripsToStart)
         controller.updateVehicles()
+
+        for vehicle in controller.parkedVehicles:
+            parkingDemand[vehicle.currentZone] += 1
+
         print(f"High priority trips: {len(controller.highPriorityTrips)}")
         print(f"Roaming vehicles: {len(controller.roamingVehicles)}")
 
@@ -66,6 +69,14 @@ print(f"Traveling vehicles: {len(controller.travelingVehicles)}")
 print(f"Google Maps Directions API Calls: {controller.gmapsClient.directionCount}")
 print(f"Google Maps Destination Matrix API Calls: {controller.gmapsClient.distanceCount}")
 print(f" => Total API Calls: {controller.gmapsClient.directionCount + controller.gmapsClient.distanceCount}")
+
+zoneMap['parking_demand'] = list(parkingDemand.values())
+
+zoneCentroids = zoneMap.geometry.centroid
+plot(zoneMap)
+plt.plot(zoneCentroids[49].x, zoneCentroids[49].y, marker='o', markersize=3, color="red")
+
+plt.show()
 
 printDebug = False
 if (printDebug):
